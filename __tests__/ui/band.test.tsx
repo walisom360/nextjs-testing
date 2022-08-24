d1f7c3923295359cbbb351c5d856c8dba406ed40
walison matheus <walisom360@hotmail.com>
2022-08-23 22:48:46 -0300

    first commit

--- __tests__/ui/band.test.tsx
+++ __tests__/ui/band.test.tsx
@@ -1,21 +1,17 @@
+import { render, screen } from "@testing-library/react";
 
-import {render ,screen} from "@testing-library/react"
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 
+import BandComponent from "@/pages/bands/[bandId]";
 
-import {readFakeData} from "@/__tests__/__mocks__/fakeData"
+test("rendering band page", async () => {
+  const { fakeBands } = await readFakeData();
 
-import BandComponent from "@/pages/bands/[bandId]"
+  render(<BandComponent band={fakeBands[0]} error={null} />);
 
+  const heading = screen.getByRole("heading", {
+    name: /the wandering bunnies/i,
+  });
 
-
-test("rendering band page" , async () => {
-  const {fakeBands} = await readFakeData()
-  
-  
-  render(<BandComponent band={fakeBands[0]} error={null} />)
-
-  const heading = screen.getByRole("heading", {name: /the wandering bunnies/i})
-
-  expect(heading).toBeInTheDocument()
-
-})
+  expect(heading).toBeInTheDocument();
+});
--- __tests__/ui/home.test.tsx
+++ __tests__/ui/home.test.tsx
@@ -1,19 +1,19 @@
-import {render, screen} from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
 
-import Home from "@/pages/index"
-
+import Home from "@/pages/index";
 
 test("page has correct heading and image", () => {
-   render(<Home />)
+  render(<Home />);
 
-   const heading = screen.getByRole("heading", {name: "Welcome to Popular Concert Venue"})
+  const heading = screen.getByRole("heading", {
+    name: "Welcome to Popular Concert Venue",
+  });
 
-   expect(heading).toBeInTheDocument()
+  expect(heading).toBeInTheDocument();
 
+  const image = screen.getByRole("img", {
+    name: "Concert goer with hands in the shape of a heart",
+  });
 
-   const image = screen.getByRole("img", {name: "Concert goer with hands in the shape of a heart"})
-
-   
-   expect(image).toBeInTheDocument()
-
-})
+  expect(image).toBeInTheDocument();
+});
